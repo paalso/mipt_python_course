4060249fd262798843b95d55d8c2ec46b05b7a75
paalso <40a.pvl@gmail.com>
2020-09-21 17:48:13 +0300

    Version 0.1

--- 6_turtle-2/Particles/particle.py
+++ 6_turtle-2/Particles/particle.py
@@ -1,67 +1,132 @@
-class Vector:
-    def __init__(self, *coords):
-        if len(coords) == 3:
-            x, y, z = coords
-            self.coords = {"x" : x, "y" : y, "z" : z}
-        elif len(coords) == 2:
-            x, y = coords
-            self.coords = {"x" : x, "y" : y}
-        elif len(coords) == 1:
-            x = coords
-            self.coords = {"x" : x}
-        elif len(coords) == 0:
-            self.coords = dict()
-
-    def get_coords(self):
-        return tuple(self.coords.values())
-
-    def __str__(self, accuracy=2):
-
-        if len(self.coords) == 2:
-            x, y = self.coords["x"], self.coords["y"]
-            return "({:.2f}, {:.2f})".format(x, y)
-        elif len(self.coords) == 3:
-            x, y, z = self.coords["x"], self.coords["y"], self.coords["z"]
-            return f"({x}, {y}, {z})"
-
-    def __mul__(self, number):
-        new_values = map(lambda e: e * number, self.coords.values())
-        return Vector(*new_values)
-
-    def __truediv__(self, number):
-        return self * (1 / number)
-
+import vector
 
 class Particle:
     def __init__(self, pos, vel, acc, mass=1):
-        self.mass = mass
         self.pos = pos
         self.vel = vel
         self.acc = acc
+        self.mass = mass
 
     def __str__(self):
         return "(mass: {}, position: {}, velocity: {}, acceleration: {})". \
             format(str(self.mass), str(self.pos), str(self.vel), str(self.acc))
 
     def get_pos(self):
-        return self.pos.get_coords()
+        return self.pos
+
+    def get_vel(self):
+        return self.vel
+
+    def get_acc(self):
+        return self.acc
+
+    def get_mass(self):
+        return self.mass
+
+    def set_pos(self, vector):
+        self.pos = vector
+
+    def set_vel(self, vector):
+        self.vel = vector
+
+    def set_acc(self, vector):
+        self.acc = vector
+
+    def get_kinetic_energy(self):
+        return .5 * self.mass * self.vel * self.vel
+
+    def get_energy(self, potential_energy=None):
+        energy = self.get_kinetic_energy()
+        if potential_energy:
+            energy += potential_energy(self.pos, self.mass)
+        return  energy
+
+    def сinematics_info(self, accuracy=2):
+        def round_collection(coll, accuracy):
+            return tuple(map(lambda x: round(x, accuracy), coll))
+        info = (self.pos.coords(), self.vel.coords(), self.acc.coords())
+        return tuple(map(lambda t: round_collection(t, accuracy), info))
+
+    def print_сinematics_info(self, accuracy=2):
+        print(self.сinematics_info(accuracy), end=" ")
+
+    def energy_info(self, accuracy=2):
+        E, K = self.get_energy(), self.get_kinetic_energy()
+        info = (E, K, E - K)
+        return tuple(map(lambda t: round(t, accuracy), info))
+
+    def print_energy_info(self, accuracy=2):
+        print("Energy: {}, kinetic {}, potential {}".
+            format(*self.energy_info()))
 
     def move(self, dt, F=None):
-        new_pos = []
-        new_vel = []
-        for pos_coord, vel_coord, acc_coord in \
-            zip(self.pos.coords.values(), self.vel.coords.values(), self.acc.coords.values()):
-            new_pos.append(pos_coord + vel_coord * dt)
-            new_vel.append(vel_coord + acc_coord * dt)
-        self.pos = Vector(*new_pos)
-        self.vel = Vector(*new_vel)
-
+        self.pos = self.pos + self.vel * dt
+        self.vel = self.vel + self.acc * dt
         if F:
-            self.acc = F(self.pos, self.val) / self.mass
+            self.acc = F(self.pos, self.acc) / self.mass
 
-    def сinematics_info(self):
-        s = str(self)
-        return s[s.index("p"):]
+    def dissipate(self, dissipate_quot):
+        """
+        Particle теряет часть кинетической энергии,
+        E_new / E_old = 1 - dissipate_quot
+        что проявляется в уменьшении скорости """
 
-    def print_сinematics_info(self):
-        print(self.сinematics_info())
+        self.vel = self.vel * (1 - dissipate_quot) ** 0.5
+
+    def collide_surface(self, reflection_vector,
+                        correct_pos=None, dissipate_quot=0):
+        """
+        При столкновении с поверхностью:
+        - вектор скорости отражается относительно поверхности
+        - возможно, корректируется положение
+        - возможно, происходит дисипация энергии """
+        self.vel = self.vel.reflect(reflection_vector)
+        self.dissipate(dissipate_quot)
+        if correct_pos:
+            correct_pos(self)
+
+
+
+(10.82, -63.82)
+pos = vector.Vector(0, 10)
+v = vector.Vector(0.564, -6.379)
+a = vector.Vector(0, -10)
+ball = Particle(pos, v, a)
+
+def potential_energy(vector, mass=1):
+    x, y = vector.coords()
+    return 10 * y * mass
+
+
+# print(ball.energy_info())
+print()
+"""
+
+pos = vector.Vector(0, 0)
+v = vector.Vector(0, 10)
+a = vector.Vector(0, -10)
+ball = Particle(pos, v, a)
+
+
+def collision_condition(position):
+    x, y = position.coords()
+    return y <= 0
+
+collision_vector = vector.Vector(1, 0)
+
+t, dt = 0, 0.05
+
+while t < 2.5:
+    print("{:.1f}    {:.2f}".format(t, ball.get_energy(potential_energy)), end="  ")
+    ball.print_сinematics_info()
+    position = ball.get_pos()
+    ball.move(dt)
+    x, y = ball.get_pos().coords()
+    if y <= 0:
+        ball.print_сinematics_info()
+        ball.collide_surface(collision_vector)
+        print("COLLISION!")
+        ball.set_pos(vector.Vector(x, 0))
+
+    t += dt
+"""
---
+++ 6_turtle-2/Particles/vector.py
@@ -0,0 +1,73 @@
+import operator
+
+
+def is_number(x):
+    if type(x) in [int, float]:
+        return True
+    return False
+
+
+class Vector:
+    def __init__(self, *coords):
+        self.vector = coords
+
+    def coords(self):
+        return tuple(self.vector)
+
+    def __str__(self):
+        return "({})".format(", ".join(map(str, self.vector)))
+
+    def __add__(self, other):
+        return Vector(*map(operator.add, self.vector, other.coords()))
+
+    def __radd__(self, other):
+        return self + other
+
+    def __sub__(self, other):
+        return Vector(*map(operator.sub, self.vector, other.coords()))
+
+    def __rsub__(self, other):
+        return other - self
+
+    def __mul__(self, other):
+        if is_number(other):
+            return Vector(*map(lambda e: e * other, self.vector))
+        elif isinstance(other, Vector):
+            return sum(map(operator.mul, self.vector, other.coords()))
+
+    def __rmul__(self, other):
+        return self * other
+
+    def __truediv__(self, other):
+        return self * (1 / other)
+
+    def __rtruediv__(self, other):
+        return self  / other
+
+    def length(self):
+        return (sum(map(lambda e: e * e, self.vector))) ** 0.5
+
+    def print(self, acc=2):
+        rounded = map(lambda e: str(round(e, 2)) , self.vector)
+        print("{}".format(", ".join(L)))
+
+    def dist(self, other):
+        return (self - other).length()
+
+    def is_close(self, other, eps):
+        return self.dist(other) <= eps
+
+    def normalize(self):
+        return self / self.length()
+
+    def orthonormalize(self):   # for 2-dimensions only
+        x, y = self.vector
+        orthogonal = Vector(-y, x)
+        return orthogonal.normalize()
+
+    def reflect(self, other):        # for 2-dimensions only
+    # Отразить заданный вектор относительно other
+        return (self * other.normalize()) * other.normalize() - \
+                (self * other.orthonormalize()) * other.orthonormalize()
+
+
