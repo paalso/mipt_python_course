880411e43a8857599b3b37ed5b79851e97dca00d
paalso <40a.pvl@gmail.com>
2021-02-19 21:01:54 +0200

    Pictures at an Exhibition 1. Task 1 OK

---
+++ 7_pictures_1/1.py
@@ -0,0 +1,114 @@
+# Задание №1 (пробное)
+# http://cs.mipt.ru/python/lessons/lab4.html#id6
+
+import pygame
+from pygame.draw import *
+
+
+def draw_eye(screen, eye_color, eye_apple_color, eye_apple_coords,
+            eye_radis, eye_apple_radis):
+    circle(screen, eye_color, eye_apple_coords, eye_radis)
+    circle(screen, BLACK, eye_apple_coords, eye_radis, 1)
+    circle(screen, eye_apple_color, eye_apple_coords, eye_apple_radis)
+
+
+def draw_inclined_rect(screen, color,
+        top_left_coords, to_axis_x_angle_grads, length, width):
+
+    import math
+    to_axis_x_angle_rads = math.radians(to_axis_x_angle_grads)
+
+    x, y = top_left_coords
+    bottom_left_coords = (x + width * math.sin(to_axis_x_angle_rads),
+                        y + width * math.cos(to_axis_x_angle_rads))
+
+    top_right_coords = (x + length * math.cos(to_axis_x_angle_rads),
+                        y - length * math.sin(to_axis_x_angle_rads))
+
+    x1, y1 = top_right_coords
+    bottom_right_coords = (x1 + width * math.sin(to_axis_x_angle_rads),
+                        y1 + width * math.cos(to_axis_x_angle_rads))
+
+    polygon(screen, color, (top_left_coords, top_right_coords,
+            bottom_right_coords, bottom_left_coords))
+
+
+pygame.init()
+FPS = 10
+screen_sz = 500
+pygame.display.set_caption("An angry humanoid")
+
+BLACK = (0, 0, 0)
+RED = (0xff, 0, 0)
+YELLOW = (0xff, 0xff, 0)
+
+# Фон
+screen = pygame.display.set_mode((screen_sz, screen_sz))
+background_color = (0xd9, 0xd9, 0xd9)   # светло-серый
+screen.fill(background_color)
+
+center_x, center_y, radius = screen_sz // 2, screen_sz // 2, screen_sz // 4
+
+# Собственно рисуем рожицу
+
+# Лицо
+face_color = YELLOW
+circle(screen, face_color, (center_x, center_y), radius)
+circle(screen, BLACK,
+        (center_x, center_y), radius, 3)
+
+# Глаза
+eye1_apple_x = center_x - 0.5 * radius
+eye2_apple_x = center_x + 0.5 * radius
+eye_apple_y = center_y - 0.2 * radius
+eye_apple_radius = 0.1 * radius
+eye_radis = 0.2 * radius
+eye_color = RED
+eye_apple_color = BLACK
+
+draw_eye(screen, eye_color, eye_apple_color, (eye1_apple_x, eye_apple_y),
+        eye_radis, eye_apple_radius)
+draw_eye(screen, eye_color, eye_apple_color, (eye2_apple_x, eye_apple_y),
+        eye_radis, eye_apple_radius)
+
+# Рот
+mouth_left_x = eye1_apple_x
+mouth_top_y = center_y + 0.5 * radius
+mouth_length = radius
+mouth_width = 0.2 * radius
+mouth_color = BLACK
+rect(screen, mouth_color,
+        ((mouth_left_x, mouth_top_y), (mouth_length, mouth_width)))
+
+# Брови
+eyebrow_width = 0.1 * radius
+
+eyebrow1_length = 0.95 * radius
+eyebrow1_top_left_coords = center_x - 0.98 * radius, center_y - 0.82 * radius
+eyebrow1_to_axis_x_angle_grads = -30
+
+eyebrow2_length = 0.87 * radius
+eyebrow2_to_axis_x_angle_grads = 20
+eyebrow2_top_left_coords = center_x + 0.18 * radius, center_y - 0.40 * radius
+
+draw_inclined_rect(screen, BLACK, eyebrow1_top_left_coords,
+        eyebrow1_to_axis_x_angle_grads, eyebrow1_length, eyebrow_width)
+
+draw_inclined_rect(screen, BLACK, eyebrow2_top_left_coords,
+        eyebrow2_to_axis_x_angle_grads, eyebrow2_length, eyebrow_width)
+
+##draw_inclined_rect(screen, BLACK, (center_x, center_y),
+##        eyebrow2_to_axis_x_angle_grads, eyebrow2_length, eyebrow_width)
+
+pygame.display.update()
+
+clock = pygame.time.Clock()
+finished = False
+
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+
+pygame.quit()
